17ad6891a53fcd4f26bc741476b5c330628a8763
fhsiao <frank.hsiao@hotmail.com>
2018-07-19 01:26:10 -0600

    HelloJNA: Add Java and load library

--- src/main/java/HelloJNA.java
+++ src/main/java/HelloJNA.java
@@ -1,8 +1,14 @@
+import com.sun.jna.*;
 
 public class HelloJNA {
 
+    public interface sayDLL extends Library {
+        sayDLL INSTANCE = (sayDLL) Native.loadLibrary("sayDLL", HelloJNA.class);
+        void helloWorld();
+    }
 
     public static void main(String[] args) {
-
+        sayDLL say = sayDLL.INSTANCE;
+        say.helloWorld();  // call of void function
     }
 }
